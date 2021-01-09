6f31b81f150d2f7122a155b16f31927e3ca6c89d
maksee <kaz.balos@gmail.com>
2021-01-10 00:19:18 +0100

    Update to recent smb client

--- nas/mount.sh
+++ nas/mount.sh
@@ -6,10 +6,12 @@
 #
 # Package required: cifs-utils
 
-VERSION=1.0.9
+VERSION=1.0.10
 MOUNT_CFG=$HOME/.mount.cfg
 ACCESS_TYPE=ro
 BASE_DIR=$HOME/net
+# vol0, vol1, ...
+START_IDX=0
 
 if [ ! -f ~/.mount.cfg ]
 then
@@ -17,11 +19,11 @@ then
 	exit 1
 fi 
 
-MOUNT_USER=`cat $MOUNT_CFG | grep user | cut -d= -f2`
-MOUNT_PASS=`cat $MOUNT_CFG | grep pass | cut -d= -f2`
+MOUNT_USER=`cat $MOUNT_CFG | grep username | cut -d= -f2`
+MOUNT_PASS=`cat $MOUNT_CFG | grep password | cut -d= -f2`
 
 function count_volumes() {
-	VOL_COUNT=`smbclient -L //$HOST.local -U $MOUNT_USER -N 2>&1 | grep ${PATTERN}.${ACCESS_TYPE} -c`
+	VOL_COUNT=`smbclient -L //$HOST.local -U $MOUNT_USER -A $MOUNT_CFG 2>&1 | grep ${PATTERN}.${ACCESS_TYPE} -c`
 	echo "${VOL_COUNT}"
 }
 
@@ -50,7 +52,7 @@ function create_dir() {
 		mkdir -p $1
 		if [ $? -eq 0 ]
 		then
-			#echo "Directory $1 created successfully"
+			echo "Directory $1 created successfully"
 			echo -n ""
 		else
 			echo "Directory $1 could not be created"
@@ -74,11 +76,8 @@ if [ $# -eq 2 -o $# -eq 3 ]
 then
 	echo "$0 version $VERSION"
 	HOST=$1
-	PATTERN=fa
-	if [ $HOST == "nas" ]
-	then
-		PATTERN=vol
-	fi
+	PATTERN=vol
+	echo "HOST=$HOST PATTERN=$PATTERN"
 else 
 	print_usage
 	exit 0
@@ -90,27 +89,22 @@ then
 fi
 
 case $2 in 
-home-start)
-	sudo mount -t cifs //$HOST.local/$MOUNT_USER $BASE_DIR/$HOST/$MOUNT_USER -ousername=$MOUNT_USER,password=$MOUNT_PASS
-;;
-home-stop)
-	sudo umount $BASE_DIR/$HOST/$MOUNT_USER
-;;
 start)
-	VOL_COUNT=$(count_volumes)
+	VOL_COUNT=`expr $(count_volumes) - 1`
 	echo "Number of volumes detected: ${VOL_COUNT}"
-	for i in `seq 1 $VOL_COUNT`
+	for i in `seq $START_IDX $VOL_COUNT`
 	do
 		DIR=${PATTERN}${i}
 		DIR_FULL=$BASE_DIR/${HOST}/${DIR}
 		create_dir ${DIR_FULL}
+		#echo "sudo mount -t cifs //$HOST.local/${DIR}${ACCESS_TYPE} ${DIR_FULL} -ousername=$MOUNT_USER,password=$MOUNT_PASS"
 		sudo mount -t cifs //$HOST.local/${DIR}${ACCESS_TYPE} ${DIR_FULL} -ousername=$MOUNT_USER,password=$MOUNT_PASS
 	done
 ;;
 stop)
-	VOL_COUNT=$(count_volumes)
+	VOL_COUNT=`expr $(count_volumes) - 1`
 	echo "Number of volumes detected: ${VOL_COUNT}"
-	for i in `seq 1 $VOL_COUNT`
+	for i in `seq $START_IDX $VOL_COUNT`
 	do
 		DIR=${PATTERN}${i}
 		DIR_FULL=$BASE_DIR/${HOST}/${DIR}
@@ -126,6 +120,8 @@ stop)
 	done
 	echo -n "Directory ${BASE_DIR}/${HOST}:"
 	remove_dir $BASE_DIR/$HOST
+	echo -n "Directory ${BASE_DIR}:"
+	remove_dir $BASE_DIR
 ;;
 *)
 	print_usage
