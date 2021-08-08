6dd1768414c484065b2c44b1d2166b923076fb7e
maksee <kaz.balos@gmail.com>
2021-08-08 23:15:10 +0200

    added ethtool to required packages

--- nas/pkgs.sh
+++ nas/pkgs.sh
@@ -7,7 +7,7 @@ function check_package() {
                 missing_pkgs="${missing_pkgs} $1"
         fi
 }
-for i in smbclient cifs-utils lsscsi smartmontools hddtemp lm-sensors nvme-cli wcstools
+for i in smbclient cifs-utils lsscsi smartmontools hddtemp lm-sensors nvme-cli wcstools ethtool
 do
         check_package $i
 done
