052cfa5a23cb15ecffd78b774f4b592b5e1fad02
maksee <kaz.balos@gmail.com>
2021-08-22 09:06:21 +0200

    Find duplicates error checking

--- chia/find-dup.sh
+++ chia/find-dup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 filename `find /mnt -name "*.plot"` > plots.txt
-filename `find $HOME/net -name "*.plot"` >> plots.txt
+if [ -e $HOME/net ] && [ `ls -A $HOME/net` ]
+then
+	filename `find $HOME/net -name "*.plot"` >> plots.txt
+fi
 cat plots.txt | sort | uniq > plots-uniq.txt
 cat plots.txt | sort > plots-sort.txt
 diff plots-uniq.txt plots-sort.txt
-rm -v plots.txt plots-sort.txt plots-uniq.txt
+rm plots.txt plots-sort.txt plots-uniq.txt
