08ad4c23108c28df0f1558976f26277c1aa6a646
maksee <kaz.balos@gmail.com>
2021-08-22 09:07:39 +0200

    find all - error checking

--- chia/find-all.sh
+++ chia/find-all.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 filename `find /mnt -name "*.plot"` | grep -c plot
-filename `find $HOME/net -name "*.plot"` | grep -c plot
+if [ -e $HOME/net ] && [ `ls -A $HOME/net` ]
+then
+	filename `find $HOME/net -name "*.plot"` | grep -c plot
+fi
